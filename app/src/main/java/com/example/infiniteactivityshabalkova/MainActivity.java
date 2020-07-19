d156655273414592ad75daa8e17d94a1baca3d96
MashaShe <shabalkova@gmail.com>
2020-07-19 15:03:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/infiniteactivityshabalkova/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.infiniteactivityshabalkova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Random random = new Random();
+        int rand = random.nextInt(99)+1;
+        final TextView myLink = findViewById(R.id.linkText);
+        myLink.setText("http://myfile.org/" + rand);
+
+        Button forwardButton = findViewById(R.id.forwardButton);
+        forwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class); //создание Intent
+                startActivity(intent); //запуск нового экрана
+
+
+            }
+        });
+
+        Button backwardsButton = findViewById(R.id.backwardsButton);
+        backwardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
